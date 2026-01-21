b7143c7675e7e0441df070b3e79a9659d300e0be
Kiet Pham <phamkiet173@gmail.com>
2026-01-21 23:31:34 +0700

    implement docks presentation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Navbar from '#components/Navbar'
 import Welcome from '#components/welcome'
+import Dock from '#components/Dock';
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock };
