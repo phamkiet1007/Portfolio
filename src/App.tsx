293a8d2fd8b7604cba9c0663e85561d58d6a7386
Kiet Pham <phamkiet173@gmail.com>
2026-01-21 15:45:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from '#components/Navbar'
+import './index.css'
+
+function App() {
+  return (
+    <main>
+      <Navbar></Navbar>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Welcome to my portfolio</h1>
+      </div>
+    </main>
+  )
+}
+
+export default App
